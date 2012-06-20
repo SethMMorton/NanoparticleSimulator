984f7cff185e12722b148d324142d5fb0e1f7313
Seth M Morton <seth.m.morton@gmail.com>
2012-06-20 12:11:36 -0400

    Finished Mie theory code

--- include/solvers.h
+++ include/solvers.h
@@ -12,4 +12,18 @@ int quasi (int nlayers,
            double *scat,
            double *absorb);
 
+/* Mie theory */
+int mie (int nlayers,
+         double complex refrac_indx[nlayers],
+         double rel_rad[nlayers],
+         double size_param,
+         double *extinct,
+         double *scat,
+         double *absorb,
+         double *backscat,
+         double *rad_pressure,
+         double *albedo,
+         double *asymmetry
+        );
+
 #endif // SOLVERS_H
